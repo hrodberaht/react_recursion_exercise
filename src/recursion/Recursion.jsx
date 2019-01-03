df3dcc15020f6d742d41f23af08c91819449764e
hrodberaht <robpszczolka@gmail.com>
2019-01-03 13:54:55 +0100

    WIP

--- src/recursion/Recursion.jsx
+++ src/recursion/Recursion.jsx
@@ -3,16 +3,32 @@ import RecursiveComponent from './RecursiveComponent';
 
 // write components here
 // const One and so on...
+const One = class One extends Component {
+  render() {
+    return <div className="box">One</div>;
+  }
+};
 
+const Two = class Two extends Component {
+  render() {
+    return <div className="box">Two</div>;
+  }
+};
+
+const Three = class Three extends Component {
+  render() {
+    return <div className="box">Three</div>;
+  }
+};
 // assign components below
-const components = [];
+const components = [One, Two, Three];
 
 export default class Recursion extends Component {
-	render() {
-  	return (
-    	<div className='App-wrapper'>
+  render() {
+    return (
+      <div className="App-wrapper">
         <RecursiveComponent components={components} />
       </div>
     );
   }
-};
+}
--- src/recursion/RecursiveComponent.jsx
+++ src/recursion/RecursiveComponent.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 export default class RecursiveComponent extends Component {
-
-	// write recursive method here
+  // write recursive method here
+  recursive = n => {
+    // return this.props.components.map(comp => {
+    const Comp = this.props.components[n];
+    if (n <= 0) {
+      return <Comp />;
+    } else {
+      return <Comp>{this.recursive(n - 1)}</Comp>;
+    }
+  };
+  // );
+  // };
 
   render() {
     return (
       <div>
         {/* invoke recursive method here */}
+        {this.recursive(2)}
       </div>
     );
   }
-};
+}
